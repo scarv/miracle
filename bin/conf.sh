d3ad15a1ac8ece265fe6dd2672811886b56d7332
Ben Marshall <ben.marshall@bristol.ac.uk>
2019-05-02 16:01:11 +0100

    Adding scale submodule dependencies
     On branch master
     Changes to be committed:
    	new file:   .gitmodules
    	new file:   bin/conf.sh
    	new file:   external/scale-hw
    	new file:   external/scale-sw

---
+++ bin/conf.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "------------------------- Project Setup -------------------------------"
+
+export UAS_ROOT=$PWD
+export SCALE_SW=$UAS_ROOT/external/scale-sw
+export SCALE_HW=$UAS_ROOT/external/scale-hw
+
+echo "UAS_ROOT = $UAS_ROOT"
+echo "SCALE_SW = $SCALE_SW"
+echo "SCALE_HW = $SCALE_HW"
+
+echo "-----------------------------------------------------------------------"
