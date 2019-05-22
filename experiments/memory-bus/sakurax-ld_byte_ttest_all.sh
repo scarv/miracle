0cfba5afc5d7c9d4025910c9e5f7617fd08ca183
Ben Marshall <ben.marshall@bristol.ac.uk>
2019-05-22 09:10:27 +0100

    Experiment and flow files for memory-bus/ld_byte
    - Specialised Python TTest script for altering the number of fixed
      bytes used in the ttest.
    - Shell scripts for automating 0-8 fixed bytes collection across
      scale and fpga targets.
    
     On branch master
     Changes to be committed:
    	new file:   experiments/memory-bus/ld_byte_ttest.py
    	new file:   experiments/memory-bus/sakurax-ld_byte_ttest_all.sh
    	new file:   experiments/memory-bus/scale-ld_byte_ttest_all.sh
    
     Changes not staged for commit:
    	modified:   external/scale-hw (modified content)

---
+++ experiments/memory-bus/sakurax-ld_byte_ttest_all.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+#
+# For each FPGA target, run multiple load-byte ttest experiments for
+# different numbers of fixed bytes.
+#
+# Make sure to *source* this script from $UAS_ROOT to make sure it
+# will run properly.
+#
+
+#
+# TTests
+# arg 0 - target platform
+# arg 1 - number of fixed bytes
+# arg 2 - serial port.
+function run_ttest {
+make -B USB_PORT=$3 \
+        TTEST_NAME=ld_byte50k_$2 \
+        TTEST_FLAGS="--fixed-byte-len $2 --fixed-value 0x73b2ccfd6a39f20f" \
+        TTEST_NUM_TRACES=50000 \
+        TTEST_CAPTURE=./experiments/memory-bus/ld_byte_ttest.py\
+        ttest_$1_memory-bus-ld_byte
+}
+
+
+# Exit on first failed command
+set -e
+
+# Run from the right place
+cd $UAS_ROOT
+
+# Make sure
+pwd
+
+#
+# MB3 Target
+#
+make -B -f Makefile.experiment USB_PORT=$1 UAS_EXPERIMENT=memory-bus/ld_byte UAS_TARGET=sakurax_mb3 build
+make -B -f Makefile.experiment USB_PORT=$1 UAS_EXPERIMENT=memory-bus/ld_byte UAS_TARGET=sakurax_mb3 program
+run_ttest sakurax_mb3 0 $1
+run_ttest sakurax_mb3 1 $1
+run_ttest sakurax_mb3 2 $1
+run_ttest sakurax_mb3 3 $1
+run_ttest sakurax_mb3 4 $1
+run_ttest sakurax_mb3 5 $1
+run_ttest sakurax_mb3 6 $1
+run_ttest sakurax_mb3 7 $1
+
+#
+# MB5 Target
+#
+make -B -f Makefile.experiment USB_PORT=$1 UAS_EXPERIMENT=memory-bus/ld_byte UAS_TARGET=sakurax_mb5 build
+make -B -f Makefile.experiment USB_PORT=$1 UAS_EXPERIMENT=memory-bus/ld_byte UAS_TARGET=sakurax_mb5 program
+run_ttest sakurax_mb5 0 $1
+run_ttest sakurax_mb5 1 $1
+run_ttest sakurax_mb5 2 $1
+run_ttest sakurax_mb5 3 $1
+run_ttest sakurax_mb5 4 $1
+run_ttest sakurax_mb5 5 $1
+run_ttest sakurax_mb5 6 $1
+run_ttest sakurax_mb5 7 $1
+
+#
+# MB8 Target
+#
+make -B -f Makefile.experiment USB_PORT=$1 UAS_EXPERIMENT=memory-bus/ld_byte UAS_TARGET=sakurax_mb8 build
+make -B -f Makefile.experiment USB_PORT=$1 UAS_EXPERIMENT=memory-bus/ld_byte UAS_TARGET=sakurax_mb8 program
+run_ttest sakurax_mb8 0 $1
+run_ttest sakurax_mb8 1 $1
+run_ttest sakurax_mb8 2 $1
+run_ttest sakurax_mb8 3 $1
+run_ttest sakurax_mb8 4 $1
+run_ttest sakurax_mb8 5 $1
+run_ttest sakurax_mb8 6 $1
+run_ttest sakurax_mb8 7 $1
+
+# Return to previous cwd
+cd -
+
+# Finish
+echo " --- FINISHED ---"
+
