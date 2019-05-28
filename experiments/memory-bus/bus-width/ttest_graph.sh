977b5e6c00c377351b1350dfd25ad03ff23964a9
Ben Marshall <ben.marshall@bristol.ac.uk>
2019-05-28 14:02:09 +0100

    Fix graph paths

--- experiments/memory-bus/bus-width/ttest_graph.sh
+++ experiments/memory-bus/bus-width/ttest_graph.sh
@@ -14,11 +14,11 @@ $UAS_ROOT/external/fw-acquisition/ttest_multi_analyse.py \
     --graph-ttest ${GRAPH}-ttest.svg \
     --graph-avg-random-trace ${GRAPH}-avg-random.svg \
     --graph-avg-fixed-trace ${GRAPH}-avg-fixed.svg \
-    -t lb0 $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}_0/ttest-fixed.trs $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}_0/ttest-random.trs \
-    -t lb1 $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}_1/ttest-fixed.trs $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}_1/ttest-random.trs \
-    -t lb2 $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}_2/ttest-fixed.trs $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}_2/ttest-random.trs \
-    -t lb3 $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}_3/ttest-fixed.trs $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}_3/ttest-random.trs \
-    -t lb4 $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}_4/ttest-fixed.trs $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}_4/ttest-random.trs \
-    -t lb5 $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}_5/ttest-fixed.trs $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}_5/ttest-random.trs \
-    -t lb6 $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}_6/ttest-fixed.trs $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}_6/ttest-random.trs \
-    -t lb7 $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}_7/ttest-fixed.trs $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}_7/ttest-random.trs 
+    -t lb0 $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}/0/ttest-fixed.trs $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}/0/ttest-random.trs \
+    -t lb1 $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}/1/ttest-fixed.trs $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}/1/ttest-random.trs \
+    -t lb2 $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}/2/ttest-fixed.trs $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}/2/ttest-random.trs \
+    -t lb3 $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}/3/ttest-fixed.trs $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}/3/ttest-random.trs \
+    -t lb4 $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}/4/ttest-fixed.trs $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}/4/ttest-random.trs \
+    -t lb5 $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}/5/ttest-fixed.trs $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}/5/ttest-random.trs \
+    -t lb6 $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}/6/ttest-fixed.trs $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}/6/ttest-random.trs \
+    -t lb7 $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}/7/ttest-fixed.trs $UAS_ROOT/build/${EXPERIMENT}/$TARGET/${TTEST_NAME}/7/ttest-random.trs 
