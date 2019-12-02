85ab66fea582a577628ed58951de8706ff1eb074
Ben Marshall <ben.marshall@bristol.ac.uk>
2019-12-02 10:42:27 +0000

    Fix ARMv7m macros for store instructions

--- tools/kernel-gen/kernel-macros-armv7m.h
+++ tools/kernel-gen/kernel-macros-armv7m.h
@@ -15,9 +15,9 @@
 #define LOAD_UBYTE_RI(RD,RB,IMM)  ldrb RD,[RB,f(__hash)IMM]
 #define LOAD_UHALF_RI(RD,RB,IMM)  ldrh RD,[RB,f(__hash)IMM]
 #define LOAD_WORD_RI(RD,RB,IMM)   ldr  RD,[RB,f(__hash)IMM]
-#define STORE_BYTE_RI(RS,RB,IMM)  strb RD,[RB,f(__hash)IMM]
-#define STORE_HALF_RI(RS,RB,IMM)  strh RD,[RB,f(__hash)IMM]
-#define STORE_WORD_RI(RS,RB,IMM)  str  RD,[RB,f(__hash)IMM]
+#define STORE_BYTE_RI(RS,RB,IMM)  strb RS,[RB,f(__hash)IMM]
+#define STORE_HALF_RI(RS,RB,IMM)  strh RS,[RB,f(__hash)IMM]
+#define STORE_WORD_RI(RS,RB,IMM)  str  RS,[RB,f(__hash)IMM]
 #define XOR_RR(RD,RA,RM)          eor  RD,RM 
 #define XOR_RI(RD,RA,IMM)         .error "Not implemented XOR RRI ARM7-M"
 #define ADD_RR(RD,RN,RM )         add  RD,RN,RM  
