88294eee3cb00bec77c28b6de1f4449ab49d5324
Ben Marshall <ben.marshall@bristol.ac.uk>
2019-12-05 09:10:31 +0000

    OpenOCD programming support for scale_lpc1313fbd48
     On branch master
     Changes to be committed:
    	modified:   target/scale_lpc1313fbd48/program/program.mk

--- target/scale_lpc1313fbd48/program/program.mk
+++ target/scale_lpc1313fbd48/program/program.mk
@@ -1,4 +1,17 @@
 
 program: $(call map_experiment_hex)
+    ifeq ($(PROGRAM_WITH_OPENOCD),0)
 	lpc21isp -wipe -hex $< $(USB_PORT) $(USB_BAUD) 12000
+    else
+	$(OPENOCD) \
+	    -f interface/jlink.cfg              \
+	    -c "transport select swd"               \
+	    -f    target/lpc13xx.cfg                \
+	    -c       "init"                         \
+	    -c "reset init"                         \
+	    -c "flash write_image erase $(call map_experiment_hex)" \
+	    -c "reset  run"                         \
+	    -c "shutdown"                           \
+        -c "exit"
+    endif
 
