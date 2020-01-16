fad0fed37685fbac25bdd7e67a6a15e4a8bca7b5
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-01-16 14:45:29 +0000

    Pipeline register operand reports
     On branch master
     Changes to be committed:
    	modified:   tools/browser/app/reports/__init__.py
    	modified:   tools/browser/app/templates/reports-landing.html
    	new file:   tools/browser/app/templates/reports-pipeline-registers.html

--- tools/browser/app/reports/__init__.py
+++ tools/browser/app/reports/__init__.py
@@ -98,8 +98,6 @@ def memory_access_hazards_plot(target_name, ename):
     normalise_axes  = request.args.get("normalise_axes","true")
     trace_type      = request.args.get("trace_type","cpa-hd")
 
-    print(trace_type)
-    
     hw_traces   = results.getTracesOfType(trace_type)
     hw_traces.sort(key=operator.attrgetter('name'))
     
@@ -117,3 +115,54 @@ def memory_access_hazards_plot(target_name, ename):
     rsp         = pd.makePlotResponse()
 
     return        rsp
+
+
+@bp.route("/pipeline-registers")
+def pipeline_registers():
+    """
+    Render the pipeline registers widths report.
+    """
+    selected_target = request.args.get("target",None)
+    trace_type      = request.args.get("trace_type","ttrace")
+    normalise_axes  = request.args.get("normalise_axes","true")
+
+    if(selected_target):
+        selected_target = bp.targets[selected_target]
+
+    return render_template(
+        "reports-pipeline-registers.html",
+        targets = bp.targets,
+        target  = selected_target,
+        trace_type = trace_type,
+        normalise_axes = normalise_axes
+    )
+
+@bp.route("/pipeline-registers/plot/<string:target_name>/<string:ename>")
+def pipeline_registers_plot(target_name, ename):
+    target      = bp.targets[target_name]
+    experiment_l= bp.experiments["pipeline/"+ename+"-lhs"]
+    experiment_r= bp.experiments["pipeline/"+ename+"-rhs"]
+    results_l   = experiment_l.getResultsForTarget(target.target_name)
+    results_r   = experiment_r.getResultsForTarget(target.target_name)
+    normalise_axes  = request.args.get("normalise_axes","true")
+    trace_type      = request.args.get("trace_type","cpa-hd")
+    
+    hw_traces   = results_l.getTracesOfType(trace_type) + \
+                  results_r.getTracesOfType(trace_type)
+    hw_traces.sort(key=operator.attrgetter('name'))
+    
+    pd          = PlotDescription(series=hw_traces)
+
+    pd.height   = 1.5*len(hw_traces)
+    pd.width    = 10
+    pd.separate_axes = True
+
+    if(normalise_axes=="true"):
+        pd.set_y_limits = True
+        pd.y_limit_min  = 0.0
+        pd.y_limit_max  = 1.0
+
+    rsp         = pd.makePlotResponse()
+
+    return        rsp
+
