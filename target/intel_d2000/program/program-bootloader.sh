b9210541930a90d23d9d9ca91e54844fa59721d0
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-02-10 09:50:57 +0000

    Intel D2000: Script to program bootloader
     On branch dev/intel_d2000
     Changes to be committed:
    	new file:   target/intel_d2000/program/program-bootloader.sh
    
     Changes not staged for commit:
    	modified:   experiments/memory-bus/bus-width-ld-word/arch/IA32_X86.S
    	modified:   target/intel_d2000/bsp/intel_d2000_bsp.c
    	modified:   target/intel_d2000/program/program.mk

---
+++ target/intel_d2000/program/program-bootloader.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Programming Intel D2000 target with bootloader..."
+
+ISSM_DBG_DIR=$IAMCU_TOOLCHAIN_DIR/../../../../debugger/openocd
+ISSM_OPENOCD=$ISSM_DBG_DIR/bin/openocd
+ISSM_BOOT_ROM=$UAS_ROOT/target/intel_d2000/program/quark_d2000_rom.bin
+
+set -e
+set -x
+
+$ISSM_OPENOCD \
+   -f $ISSM_DBG_DIR/scripts/interface/ftdi/olimex-arm-usb-ocd-h.cfg  \
+   -f $UAS_ROOT/target/intel_d2000/program/quark_d2000_ufo.cfg \
+   -c "init" \
+   -c "reset halt" \
+   -c "set QUARK_D2000_OTPC_DATA_WRITE_ENABLED 1" \
+   -c "targets" \
+   -c "load_image $ISSM_BOOT_ROM 0x00000000" \
+   -c "reset run" \
+   -c "targets" \
+   -c "exit" ; echo "Bootloader programmed"
