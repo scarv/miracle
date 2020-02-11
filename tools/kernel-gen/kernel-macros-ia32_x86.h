5686e11696e0326c08ea048db42d1145664ebda2
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-02-11 11:24:03 +0000

    IA32_X86 - Fix kernel entry/exit macros
    - Make sure we can access variables passed on the stack properly.
    
     On branch dev/intel_d2000
     Changes to be committed:
    	modified:   tools/kernel-gen/kernel-macros-ia32_x86.h
    
     Untracked files:
    	experiments/memory-bus/registers-implicit-ld-st/arch/IA32_X86.S
    	experiments/memory-bus/registers-implicit-st-ld/arch/IA32_X86.S
    	experiments/memory-bus/registers-implicit-st-st-1/arch/IA32_X86.S
    	experiments/memory-bus/registers-implicit-st-st-2/arch/IA32_X86.S

--- tools/kernel-gen/kernel-macros-ia32_x86.h
+++ tools/kernel-gen/kernel-macros-ia32_x86.h
@@ -1,18 +1,18 @@
 
-#define FUNC_ENTER                pushl %eax        ; \
+#define FUNC_ENTER                pushl %ebp        ; \
+                                  movl  %esp, %ebp  ; \
+                                  pushl %eax        ; \
                                   pushl %ebx        ; \
                                   pushl %ecx        ; \
                                   pushl %edx        ; \
-                                  pushl %ebp        ; \
-                                  movl  %esp, %ebp  ; \
                                   
 
-#define FUNC_RETURN               movl  %ebp, %esp  ; \
-                                  popl  %ebp        ; \
-                                  popl  %edx        ; \
+#define FUNC_RETURN               popl  %edx        ; \
                                   popl  %ecx        ; \
                                   popl  %ebx        ; \
                                   popl  %eax        ; \
+                                  movl  %ebp, %esp  ; \
+                                  popl  %ebp        ; \
                                   ret               ;
 
 #define NOP_SPACER                nop;nop;nop;nop;nop;nop;nop;nop;
