62ca05519dcc81444bb51df39ecad16c4d562a4d
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-02-11 10:10:30 +0000

    IA32_X86 Arch: Push all a/b/c/d regs to stack
    - Push all GP registers to the stack on experiment entry and pop
      them again when leaving.
    
    - Makes it easier to guaranteed that we don't trash anything valuable.
    
     On branch dev/intel_d2000
     Changes to be committed:
    	modified:   tools/kernel-gen/kernel-macros-ia32_x86.h
    
     Changes not staged for commit:
    	modified:   experiments/memory-bus/bus-width-ld-bytes/arch/IA32_X86.S
    	modified:   experiments/memory-bus/bus-width-ld-halfword/arch/IA32_X86.S
    	modified:   experiments/memory-bus/bus-width-ld-word/arch/IA32_X86.S
    
     Untracked files:
    	experiments/memory-bus/bus-width-st-bytes/arch/IA32_X86.S
    	experiments/memory-bus/bus-width-st-halfword/arch/IA32_X86.S
    	experiments/memory-bus/bus-width-st-word/arch/IA32_X86.S
    	experiments/memory-bus/registers-implicit-ld-ld/arch/IA32_X86.S

--- tools/kernel-gen/kernel-macros-ia32_x86.h
+++ tools/kernel-gen/kernel-macros-ia32_x86.h
@@ -1,10 +1,18 @@
 
-#define FUNC_ENTER                pushl %ebp        ; \
+#define FUNC_ENTER                pushl %eax        ; \
+                                  pushl %ebx        ; \
+                                  pushl %ecx        ; \
+                                  pushl %edx        ; \
+                                  pushl %ebp        ; \
                                   movl  %esp, %ebp  ; \
                                   
 
 #define FUNC_RETURN               movl  %ebp, %esp  ; \
                                   popl  %ebp        ; \
+                                  popl  %edx        ; \
+                                  popl  %ecx        ; \
+                                  popl  %ebx        ; \
+                                  popl  %eax        ; \
                                   ret               ;
 
 #define NOP_SPACER                nop;nop;nop;nop;nop;nop;nop;nop;
