287199504add0ac462b5f444abfbaf8837ca6d2e
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-02-18 11:13:56 +0000

    Allow specifying of trace set type

--- tools/database/ldb/records/TraceSet.py
+++ tools/database/ldb/records/TraceSet.py
@@ -11,11 +11,13 @@ class TraceSet(Base):
     
     __tablename__ = "trace_sets"
 
-    id          = Column(Integer, primary_key=True)
-    filepath    = Column(String)
-    experimentId= Column(Integer, ForeignKey("experiments.id"))
-    targetId    = Column(Integer, ForeignKey("targets.id"))
-    scope_samplerate = Column(Integer)
-    scope_resolution = Column(Integer)
-    trace_length     = Column(Integer)
-    device_freq      = Column(Integer)
+    id               = Column(Integer, primary_key=True)
+    set_type         = Column(String)
+    filepath_fix     = Column(String)
+    filepath_random  = Column(String)
+    experimentId     = Column(Integer, ForeignKey("experiments.id"))
+    targetId         = Column(Integer, ForeignKey("targets.id"))
+    scope_samplerate = Column(Integer, default = 0)
+    scope_resolution = Column(Integer, default = 0)
+    trace_length     = Column(Integer, default = 0)
+    device_freq      = Column(Integer, default = 0)
