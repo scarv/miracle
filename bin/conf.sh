2f597956e359b1fbc6d17e3c198e859e3104a381
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-02-18 12:20:56 +0000

    DB: Add UAS_DB[_BACKEND] project environment variables.
     On branch dev/database
     Your branch is ahead of 'origin/dev/database' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   bin/conf.sh

--- bin/conf.sh
+++ bin/conf.sh
@@ -4,6 +4,8 @@ echo "------------------------- Project Setup -------------------------------"
 
 export UAS_ROOT=$PWD
 export UAS_BUILD=$UAS_ROOT/work
+export UAS_DB=$UAS_BUILD/database.sqlite
+export UAS_DB_BACKEND=sqlite
 export SCALE_SW=$UAS_ROOT/external/scale-sw
 export SCALE_HW=$UAS_ROOT/external/scale-hw
 
@@ -31,6 +33,7 @@ mkdir -p $UAS_BUILD
 
 echo "UAS_ROOT  = $UAS_ROOT"
 echo "UAS_BUILD = $UAS_BUILD"
+echo "UAS_DB    = $UAS_DB_BACKEND:///$UAS_DB"
 echo "SCALE_SW  = $SCALE_SW"
 echo "SCALE_HW  = $SCALE_HW"
 echo "OPENOCD   = $OPENOCD"
