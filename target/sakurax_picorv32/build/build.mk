9622183ed6daa923ec155a167495a7f2874ab2ae
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-05-28 10:53:39 +0100

    Target: sakurax_picorv32 - set default RISCV path

--- target/sakurax_picorv32/build/build.mk
+++ target/sakurax_picorv32/build/build.mk
@@ -4,11 +4,13 @@ CFLAGS += -DARCH_$(TARGET_ARCH) -Wall -O2
 CFLAGS += -march=rv32imc -mabi=ilp32 -nostartfiles -mcmodel=medany
 CFLAGS += -Wl,-T -Wl,$(UAS_ROOT)/target/sakurax_picorv32/build/linker.ld
 
-CC      = $(RISCV)/bin/riscv32-unknown-elf-gcc
-LD      = $(RISCV)/bin/riscv32-unknown-elf-ld     
-AS      = $(RISCV)/bin/riscv32-unknown-elf-as     
-OBJDUMP = $(RISCV)/bin/riscv32-unknown-elf-objdump
-OBJCOPY = $(RISCV)/bin/riscv32-unknown-elf-objcopy
+RISCV  ?= /opt/riscv
+
+CC      = $(RISCV)/bin/riscv64-unknown-elf-gcc
+LD      = $(RISCV)/bin/riscv64-unknown-elf-ld     
+AS      = $(RISCV)/bin/riscv64-unknown-elf-as     
+OBJDUMP = $(RISCV)/bin/riscv64-unknown-elf-objdump
+OBJCOPY = $(RISCV)/bin/riscv64-unknown-elf-objcopy
 
 TARGET_SRCS = $(TARGET_DIR)/srcs/boot.S \
               $(TARGET_DIR)/bsp/sakurax_picorv32_bsp.c
