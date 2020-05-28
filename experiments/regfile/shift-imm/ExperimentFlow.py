3533b4e72fb6f03898d081cc02112766a14a14ef
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-05-28 15:52:39 +0100

    Improve regfile/shift-imm experiment.
     On branch master
     Your branch is ahead of 'origin/master' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   experiments/regfile/shift-imm/ExperimentFlow.py
    	modified:   experiments/regfile/shift-imm/arch/ARMV6M.S
    	modified:   experiments/regfile/shift-imm/arch/ARMV7M.S
    	modified:   experiments/regfile/shift-imm/arch/MICROBLAZE.S
    	modified:   experiments/regfile/shift-imm/arch/RV32IMC.S
    	modified:   experiments/regfile/shift-imm/experiment.c

--- experiments/regfile/shift-imm/ExperimentFlow.py
+++ experiments/regfile/shift-imm/ExperimentFlow.py
@@ -16,3 +16,17 @@ def runCapture(args):
             {}
         )
 
+
+def runAnalysis(aif):
+    """
+    Run any experiment specific analysis.
+
+    aif - AnalysisInterface instance
+    """
+
+    aif.runDefaultAnalysis()
+
+    for ttest in aif.getTTestsForTargetAndExperiment():
+        aif.runHammingDistanceAnalysis(ttest.randomTraceSet, "di1","di2")
+        aif.runHammingDistanceAnalysis(ttest.randomTraceSet, "di2","di3")
+
--- experiments/regfile/shift-imm/experiment.c
+++ experiments/regfile/shift-imm/experiment.c
@@ -18,15 +18,21 @@
 uint8_t randomness[RLEN];
 
 int di1_fixed,di1_rand ; //!< TTest random input values.
+int di2_fixed,di2_rand ; //!< TTest random input values.
+int di3_fixed,di3_rand ; //!< TTest random input values.
 
 //! Variables which the SCASS framework can control.
 scass_target_var  experiment_variables [] = {
 {"di1" , 4, &di1_rand, &di1_fixed, SCASS_FLAGS_TTEST_IN},
+{"di2" , 4, &di2_rand, &di2_fixed, SCASS_FLAGS_TTEST_IN},
+{"di3" , 4, &di3_rand, &di3_fixed, SCASS_FLAGS_TTEST_IN},
 };
 
 //! Declaration for the experiment payload function in ldst-byte.S
 extern void     * experiment_payload(
-    int d1
+    int d1,
+    int d2,
+    int d3
 );
 
 //! Does nothing.
@@ -45,11 +51,13 @@ uint8_t experiment_run(
 ){
 
     int d1 = (fixed ? di1_fixed: di1_rand);
+    int d2 = (fixed ? di2_fixed: di2_rand);
+    int d3 = (fixed ? di3_fixed: di3_rand);
 
     uas_bsp_trigger_set();
     
     experiment_payload(
-        d1
+        d1, d2, d3
     );
     
     uas_bsp_trigger_clear();
@@ -69,7 +77,7 @@ void experiment_setup_scass(
 
     cfg -> variables             = experiment_variables ;
     cfg -> variables[0].size = sizeof(int);
-    cfg -> num_variables         = 1                    ;
+    cfg -> num_variables         = 3                    ;
     cfg -> randomness            = randomness;
     cfg -> randomness_len        = RLEN;
     cfg -> randomness_refresh_rate=0;
