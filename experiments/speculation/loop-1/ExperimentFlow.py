4c400c7e5914c445ab254a519fd374e4059b4439
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-07-02 15:15:04 +0100

    Tidying speculation/loop-* experiments.
     On branch master
     Your branch is ahead of 'origin/master' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   experiments/speculation/loop-0/ExperimentFlow.py
    	modified:   experiments/speculation/loop-0/README.dox
    	modified:   experiments/speculation/loop-0/arch/ARMV6M.S
    	modified:   experiments/speculation/loop-0/arch/ARMV7M.S
    	modified:   experiments/speculation/loop-0/arch/IA32_X86.S
    	modified:   experiments/speculation/loop-0/arch/MICROBLAZE.S
    	modified:   experiments/speculation/loop-0/arch/RV32IMC.S
    	modified:   experiments/speculation/loop-1/ExperimentFlow.py
    	modified:   experiments/speculation/loop-1/README.dox
    	modified:   experiments/speculation/loop-1/arch/ARMV6M.S
    	modified:   experiments/speculation/loop-1/arch/ARMV7M.S
    	modified:   experiments/speculation/loop-1/arch/IA32_X86.S
    	modified:   experiments/speculation/loop-1/arch/MICROBLAZE.S
    	modified:   experiments/speculation/loop-1/arch/RV32IMC.S
    
     Changes not staged for commit:
    	modified:   experiments/regfile/neighbour-hw/ExperimentFlow.py
    	deleted:    experiments/regfile/neighbour-hw/README.dox
    	modified:   experiments/regfile/neighbour-hw/arch/ARMV6M.S
    	modified:   experiments/regfile/neighbour-hw/arch/ARMV7M.S
    	modified:   experiments/regfile/neighbour-hw/arch/IA32_X86.S
    	modified:   experiments/regfile/neighbour-hw/arch/MICROBLAZE.S
    	modified:   experiments/regfile/neighbour-hw/arch/RV32IMC.S
    	modified:   experiments/regfile/neighbour-hw/experiment.c
    	modified:   experiments/regfile/shift-imm/ExperimentFlow.py
    	deleted:    experiments/regfile/shift-imm/README.dox
    	modified:   experiments/regfile/shift-imm/arch/ARMV6M.S
    	modified:   experiments/regfile/shift-imm/arch/ARMV7M.S
    	modified:   experiments/regfile/shift-imm/arch/MICROBLAZE.S
    	modified:   experiments/regfile/shift-imm/arch/RV32IMC.S
    
     Untracked files:
    	experiments/regfile/neighbour-hw/README.md
    	experiments/regfile/shift-imm/README.md

--- experiments/speculation/loop-1/ExperimentFlow.py
+++ experiments/speculation/loop-1/ExperimentFlow.py
@@ -10,9 +10,23 @@ def runCapture(args):
     Top level function for running all trace capture processes for this
     experiment.
     """
-    return args.runAndInsertTTest (
-            EXPERIMENT_CATAGORY ,
-            EXPERIMENT_NAME     ,
-            {}
-        )
+
+    args.runAndInsertTraceCollection (
+        EXPERIMENT_CATAGORY ,
+        EXPERIMENT_NAME     ,
+        {}                  ,
+        int(args.num_ttest_traces)
+    )
+
+def runAnalysis(aif):
+    """
+    Run any experiment specific analysis.
+
+    aif - AnalysisInterface instance
+    """
+    
+    for blob in aif.getTraceSetBlobsForTargetAndExperiment():
+        aif.runHammingWeightAnalysis(blob, "di1")
+        aif.runHammingWeightAnalysis(blob, "di2")
+        aif.runHammingDistanceAnalysis(blob, "di1","di2")
 
