fd37a174431fd4867625593d36fa1d3d44d7ced2
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-07-02 15:38:02 +0100

    Updates to neighbour-hw - still needs work.
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	modified:   experiments/regfile/neighbour-hw/ExperimentFlow.py
    	renamed:    experiments/regfile/neighbour-hw/README.dox -> experiments/regfile/neighbour-hw/README.md
    	modified:   experiments/regfile/neighbour-hw/arch/ARMV6M.S
    	modified:   experiments/regfile/neighbour-hw/arch/ARMV7M.S
    	modified:   experiments/regfile/neighbour-hw/arch/IA32_X86.S
    	modified:   experiments/regfile/neighbour-hw/arch/MICROBLAZE.S
    	modified:   experiments/regfile/neighbour-hw/arch/RV32IMC.S
    	modified:   experiments/regfile/neighbour-hw/experiment.c

--- experiments/regfile/neighbour-hw/ExperimentFlow.py
+++ experiments/regfile/neighbour-hw/ExperimentFlow.py
@@ -10,9 +10,31 @@ def runCapture(args):
     Top level function for running all trace capture processes for this
     experiment.
     """
-    return args.runAndInsertTTest (
-            EXPERIMENT_CATAGORY ,
-            EXPERIMENT_NAME     ,
-            {}
-        )
+    args.runAndInsertTraceCollection (
+        EXPERIMENT_CATAGORY ,
+        EXPERIMENT_NAME     ,
+        {}                  ,
+        int(args.num_ttest_traces)
+    )
 
+
+def runAnalysis(aif):
+    """
+    Run any experiment specific analysis.
+
+    aif - AnalysisInterface instance
+    """
+
+    for blob in aif.getTraceSetBlobsForTargetAndExperiment():
+
+        aif.runHammingWeightAnalysis  (blob, "di1")
+        aif.runHammingWeightAnalysis  (blob, "di2")
+        aif.runHammingWeightAnalysis  (blob, "di3")
+        aif.runHammingWeightAnalysis  (blob, "di4")
+
+        aif.runHammingDistanceAnalysis(blob, "di1","di2")
+        aif.runHammingDistanceAnalysis(blob, "di1","di3")
+        aif.runHammingDistanceAnalysis(blob, "di1","di4")
+        aif.runHammingDistanceAnalysis(blob, "di2","di3")
+        aif.runHammingDistanceAnalysis(blob, "di2","di4")
+        aif.runHammingDistanceAnalysis(blob, "di3","di4")
--- experiments/regfile/neighbour-hw/experiment.c
+++ experiments/regfile/neighbour-hw/experiment.c
@@ -18,15 +18,24 @@
 uint8_t randomness[RLEN];
 
 int di1_fixed,di1_rand ; //!< TTest random input values.
+int di2_fixed,di2_rand ; //!< TTest random input values.
+int di3_fixed,di3_rand ; //!< TTest random input values.
+int di4_fixed,di4_rand ; //!< TTest random input values.
 
 //! Variables which the SCASS framework can control.
 scass_target_var  experiment_variables [] = {
 {"di1" , 4, &di1_rand, &di1_fixed, SCASS_FLAGS_TTEST_IN},
+{"di2" , 4, &di2_rand, &di2_fixed, SCASS_FLAGS_TTEST_IN},
+{"di3" , 4, &di3_rand, &di3_fixed, SCASS_FLAGS_TTEST_IN},
+{"di4" , 4, &di4_rand, &di4_fixed, SCASS_FLAGS_TTEST_IN},
 };
 
 //! Declaration for the experiment payload function in ldst-byte.S
 extern void     * experiment_payload(
-    int d1
+    int d1,
+    int d2,
+    int d3,
+    int d4
 );
 
 //! Does nothing.
@@ -45,11 +54,14 @@ uint8_t experiment_run(
 ){
 
     int d1 = (fixed ? di1_fixed: di1_rand);
+    int d2 = (fixed ? di2_fixed: di2_rand);
+    int d3 = (fixed ? di3_fixed: di3_rand);
+    int d4 = (fixed ? di4_fixed: di4_rand);
 
     uas_bsp_trigger_set();
     
     experiment_payload(
-        d1
+        d1, d2, d3, d4
     );
     
     uas_bsp_trigger_clear();
@@ -69,7 +81,7 @@ void experiment_setup_scass(
 
     cfg -> variables             = experiment_variables ;
     cfg -> variables[0].size = sizeof(int);
-    cfg -> num_variables         = 1                    ;
+    cfg -> num_variables         = 4                    ;
     cfg -> randomness            = randomness;
     cfg -> randomness_len        = RLEN;
     cfg -> randomness_refresh_rate=0;
